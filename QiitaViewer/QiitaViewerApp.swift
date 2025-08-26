3cf2387cb0375c8574bc163bc98690c16f455b2a
bookii <mizznoff+dev@gmail.com>
2025-08-26 21:35:03 +0900

    Initial Commit

---
+++ QiitaViewer/QiitaViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QiitaViewerApp.swift
+//  QiitaViewer
+//
+//  Created by bookii on 2025/08/26.
+//
+
+import SwiftUI
+
+@main
+struct QiitaViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
