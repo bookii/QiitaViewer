9c6b08d16617674f065cc411ae0d6c543cdead68
mizznoff <mizznoff+dev@gmail.com>
2025-08-27 21:00:18 +0900

    ビルド時に SwiftFormat が実行されるようにする

---
+++ BuildTools/Package.swift
@@ -0,0 +1,12 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "BuildTools",
+    dependencies: [
+        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.55.0"),
+    ],
+    targets: [.target(name: "BuildTools", path: "")]
+)
